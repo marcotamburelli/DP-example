8b2057838f2e98b101e90e64b1f1c67cac2168fb
Marco Tamburelli <marco.tamburelli@gmail.com>
2018-03-08 21:11:16 +0100

    improved listener pattern

--- src/js/renderer/app.tsx
+++ src/js/renderer/app.tsx
@@ -4,15 +4,13 @@ import { Editor } from './component/editor';
 import { Element } from './component/element';
 import { EventType } from './const';
 import { App, Item } from './domain';
-import { init } from './usecase/init';
+import { init } from './init';
 
-function itemGenerator(editor: XLib.Container<Item, HTMLDivElement>) {
-  return (data: Item) => {
-    const element = Element(editor, data.id);
+const itemGenerator = (editor: XLib.Container<Item, HTMLDivElement>) => (data: Item) => {
+  const element = Element(editor, data.id);
 
-    return element;
-  };
-}
+  return element;
+};
 
 const editor = <Editor name="editor" id="editor" />;
 
@@ -23,7 +21,7 @@ const app: XLib.Container<App, HTMLDivElement> = (
       <ul>
         <XLib.List id="list" name="items" generator={itemGenerator(editor)} />
         <li>
-          <button id="new" type="button" onclick={{ eventType: EventType.NEW_ITEM, emitter: () => null }}>add</button>
+          <button id="new" type="button" onclick={{ eventType: EventType.NEW_ITEM, emitter: () => ({}) }}>add</button>
         </li>
       </ul>
     </div>
---
+++ src/js/renderer/init.ts
@@ -0,0 +1,44 @@
+import { XLib } from 'hello';
+import { Observable } from 'rxjs/Rx';
+
+import { Element } from './component/element';
+import { EventType } from './const';
+import { Item } from './domain';
+
+export function init(button: XLib.ControlComponent<any, any>, list: XLib.ListContainer<any>, editor: XLib.Container<any, any>) {
+  const mainListener = XLib.listen(
+    Observable.from(button.createObservable<Item>())
+      .merge(Observable.from(list.createObservable<Item>()))
+  );
+
+  mainListener.on(EventType.NEW_ITEM).execute((payload) => editor.setData({}));
+  mainListener.on(EventType.EDIT_ITEM).execute((item) => editor.setData(item));
+
+  const editorListener = XLib.listen(Observable.from(editor.createObservable()));
+
+  editorListener.on(EventType.EXIT_EDITOR).execute(hideEditor(editor));
+  editorListener.on(EventType.UPDATE_ITEM).execute(createOrUpdateItem(editor, list));
+};
+
+const hideEditor = (editor: XLib.ControlComponent<any, any>) => (payload) => {
+  editor.domNode.classList.add('hidden');
+};
+
+var idx = 0;
+
+const createOrUpdateItem = (
+  editor: XLib.Container<any, any>,
+  list: XLib.ListContainer<any>
+) => (payload: Item) => {
+  if (payload.id == null) {
+    var element = Element(editor, payload.id);
+
+    payload = { ...payload, id: ++idx };
+    list.append(element);
+  } else {
+    element = list.queryById<XLib.ControlComponent<any, any>>(`item-${payload.id}`);
+  }
+
+  element.setData(payload);
+  editor.domNode.classList.add('hidden');
+};
