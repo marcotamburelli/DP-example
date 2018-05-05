e41c54e8d2af4fad7ea9c2b5696e1682522d1841
Marco Tamburelli <marco.tamburelli@gmail.com>
2018-05-06 00:30:51 +0200

    migrated to single page application

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,33 @@
-var gulp = require('gulp');
-var sourcemaps = require('gulp-sourcemaps');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var browserify = require('browserify');
-var watchify = require('watchify');
-var babel = require('babelify');
-var tsify = require('tsify');
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+const browserify = require('browserify');
+const watchify = require('watchify');
+const babel = require('babelify');
+const tsify = require('tsify');
+const connect = require('gulp-connect');
 
-function compile(watch) {
-  var bundler = watchify(
+const compile = (watch) => {
+  const bundler = watchify(
     browserify('./src/js/renderer/app.tsx', { debug: true })
       .plugin(tsify, { target: 'es6' })
-      .transform(babel, { presets: ['es2015'], extensions: ['.ts', '.tsx'] })
+      .transform(babel, { presets: ['env'], extensions: ['.ts', '.tsx'] })
   );
 
-  function rebundle() {
+  const rebundle = () => {
     bundler.bundle()
       .on('error', function (err) { console.error(err); this.emit('end'); })
-      .pipe(source('renderer.js'))
+      .pipe(source('bundle.js'))
       .pipe(buffer())
       // .pipe(uglify())
       .pipe(sourcemaps.init({ loadMaps: true }))
       .pipe(sourcemaps.write('./'))
       .pipe(gulp.dest('./html/dist'));
-  }
+  };
 
   if (watch) {
-    bundler.on('update', function () {
+    bundler.on('update', () => {
       console.log('-> bundling...');
       rebundle();
       console.log('-> done');
@@ -34,13 +35,15 @@ function compile(watch) {
   }
 
   rebundle();
-}
+};
 
-function watch() {
-  return compile(true);
-}
+const watch = () => compile(true);
 
-gulp.task('build', function () { return compile(); });
-gulp.task('watch', function () { return watch(); });
+gulp.task('build', () => compile());
+gulp.task('watch', () => watch());
+gulp.task('webserver', () => connect.server({
+  root: 'html',
+  livereload: true
+}));
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['webserver', 'watch']);
